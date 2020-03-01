6eda4c19d8b61f10bc3a5d19ef4eec0058d109ff
Ricardo TB <ricardo.jucrist@hotmail.com>
2020-03-01 08:39:46 -0300

    first commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Main() {
+    return <View />;
+}
