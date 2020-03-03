9a054d4b2b7dad9e5a42be1a24436f8bcad33f6f
Ricardo TB <ricardo.jucrist@hotmail.com>
2020-03-03 17:57:24 -0300

    finished API

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,20 @@
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import {View} from 'react-native';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+    Container,
+    Header,
+    Avatar,
+    Name,
+    Bio,
+    Stars,
+    Starred,
+    OwnerAvatar,
+    Info,
+    Title,
+    Autor,
+} from './styles';
 
 export default class User extends Component {
     state = {
@@ -21,7 +32,34 @@ export default class User extends Component {
 
     render() {
         const {stars} = this.state;
+        const {route} = this.props;
 
-        return <View />;
+        const user = route.params.user;
+
+        return (
+            <Container>
+                <Header>
+                    <Avatar source={{uri: user.avatar}} />
+                    <Name>{user.name}</Name>
+                    <Bio>{user.bio}</Bio>
+                </Header>
+
+                <Stars
+                    data={stars}
+                    keyExtractor={star => String(star.id)}
+                    renderIdem={({item}) => (
+                        <Starred>
+                            <OwnerAvatar
+                                source={{uri: item.owner.avatar_url}}
+                            />
+                            <Info>
+                                <Title>{item.name}</Title>
+                                <Autor>{item.owner.login}</Autor>
+                            </Info>
+                        </Starred>
+                    )}
+                />
+            </Container>
+        );
     }
 }
